d72faba647fd0ce29e0bd73978698f2e2ed1363e
Sam Kleinman <samk@10gen.com>
2013-01-16 19:18:46 -0500

    legacy rendering updates

--- docs/conf.py
+++ docs/conf.py
@@ -112,7 +112,7 @@ latex_documents = [
 ]
 
 latex_elements = {
-    'preamble': '\DeclareUnicodeCharacter{FF04}{\$} \DeclareUnicodeCharacter{FF0E}{.}',
+    'preamble': '\DeclareUnicodeCharacter{FF04}{\$} \DeclareUnicodeCharacter{FF0E}{.} \PassOptionsToPackage{hyphens}{url}',
     'pointsize': '10pt',
     'papersize': 'letterpaper'
 }
