3f1a951bc4b5175f472dd01c256bc80c02576abd
Tyler Brock <tyler.brock@gmail.com>
2013-01-26 17:12:35 -0500

    reordering ocd

--- ruby/spec/bson/bson_spec.rb
+++ ruby/spec/bson/bson_spec.rb
@@ -18,64 +18,64 @@ module BSON
 
   describe Float do
     it_behaves_like 'a bson element' do
-      let(:type)  { "\x01" }
       let(:obj)   { 1.2 }
+      let(:type)  { "\x01" }
       let(:value) { "333333\xF3?" }
     end
   end
 
   describe String do
     it_behaves_like 'a bson element' do
-      let(:type)  { "\x02" }
       let(:obj)   { "string" }
+      let(:type)  { "\x02" }
       let(:value) { "\a\x00\x00\x00string\x00" }
     end
   end
 
   describe Hash do
     it_behaves_like 'a bson element' do
-      let(:type)  { "\x03" } 
       let(:obj)   { { :a => 1 } }
+      let(:type)  { "\x03" }
       let(:value) { "\f\x00\x00\x00\x10a\x00\x01\x00\x00\x00\x00" }
     end
   end
 
   describe Array do
     it_behaves_like 'a bson element' do
-      let(:type)  { "\x04" } 
       let(:obj)   { ['a'] }
+      let(:type)  { "\x04" }
       let(:value) { "\x0E\x00\x00\x00\x020\x00\x02\x00\x00\x00a\x00\x00" }
     end
   end
 
   describe Binary do
     it_behaves_like 'a bson element' do
-      let(:type)  { "\x05" } 
       let(:obj)   { Binary.new("a") }
+      let(:type)  { "\x05" }
       let(:value) { "\x01\x00\x00\x00\x00a" }
     end
   end
 
   describe Undefined do
     it_behaves_like 'a bson element' do
-      let(:type)  { "\x06" } 
       let(:obj)   { Undefined }
+      let(:type)  { "\x06" }
       let(:value) { "" }
     end
   end
 
   describe FalseClass do
     it_behaves_like "a bson element" do
-      let(:type)  { "\x08" }
       let(:obj)   { false }
+      let(:type)  { "\x08" }
       let(:value) { "\x00" }
     end
   end
 
   describe TrueClass do
     it_behaves_like 'a bson element' do
-      let(:type)  { "\x08" }
       let(:obj)   { true }
+      let(:type)  { "\x08" }
       let(:value) { "\x01" }
     end
   end
