7b1a55e5281ea1938b0afa32d3cf0f1331e50836
Durran Jordan <durran@gmail.com>
2013-02-26 14:37:59 +0100

    Adding in ruby integer ext specs

--- ruby/lib/bson/ext/integer.rb
+++ ruby/lib/bson/ext/integer.rb
@@ -15,17 +15,88 @@ module BSON
       # @since 2.0.0
       INT32_PACK = "l".freeze
 
+      # Constant for the int 64 pack directive.
+      #
+      # @since 2.0.0
+      INT64_PACK = "q".freeze
+
       # A 32bit integer is type 0x10 in the BSON spec.
       #
       # @since 2.0.0
       INT32_TYPE = 16.chr.freeze
 
+      # A 64bit integer is type 0x12 in the BSON spec.
+      #
+      # @since 2.0.0
+      INT64_TYPE = 18.chr.freeze
+
+      # The maximum 32 bit integer value.
+      #
+      # @since 2.0.0
+      MAX_32BIT = (1 << 31) - 1
+
+      # The maximum 64 bit integer value.
+      #
+      # @since 2.0.0
+      MAX_64BIT = (1 << 63) - 1
+
+      # The minimum 32 bit integer value.
+      #
+      # @since 2.0.0
+      MIN_32BIT = -(1 << 31)
+
+      # The minimum 64 bit integer value.
+      #
+      # @since 2.0.0
+      MIN_64BIT = -(1 << 63)
+
+      # Is this integer a valid BSON 32 bit value?
+      #
+      # @example Is the integer a valid 32 bit value?
+      #   1024.bson_int32?
+      #
+      # @return [ true, false ] If the integer is 32 bit.
+      #
+      # @since 2.0.0
+      def bson_int32?
+        (MIN_32BIT <= self) && (self <= MAX_32BIT)
+      end
+
+      # Is this integer a valid BSON 64 bit value?
+      #
+      # @example Is the integer a valid 64 bit value?
+      #   1024.bson_int64?
+      #
+      # @return [ true, false ] If the integer is 64 bit.
+      #
+      # @since 2.0.0
+      def bson_int64?
+        (MIN_64BIT <= self) && (self <= MAX_64BIT)
+      end
+
+      # Get the BSON type for this integer. Will depend on whether the integer
+      # is 32 bit or 64 bit.
+      #
+      # @example Get the BSON type for the integer.
+      #   1024.bson_type
+      #
+      # @return [ String ] The single byte BSON type.
+      #
+      # @see http://bsonspec.org/#/specification
+      #
+      # @since 2.0.0
       def bson_type
-        INT32_TYPE
+        bson_int32? ? INT32_TYPE : (bson_int64? ? INT64_TYPE : nil)
       end
 
       def to_bson
-        [ self ].pack(INT32_PACK)
+        [ self ].pack(bson_pack_directive)
+      end
+
+      private
+
+      def bson_pack_directive
+        bson_int32? ? INT32_PACK : (bson_int64? ? INT64_PACK : nil)
       end
     end
 
---
+++ ruby/spec/bson/ext/integer_spec.rb
@@ -0,0 +1,169 @@
+require "spec_helper"
+
+describe BSON::Ext::Integer do
+
+  describe "::INT32_PACK" do
+
+    it "returns l" do
+      expect(Integer::INT32_PACK).to eq("l")
+    end
+  end
+
+  describe "::INT32_TYPE" do
+
+    it "returns 0x10" do
+      expect(Integer::INT32_TYPE).to eq(16.chr)
+    end
+  end
+
+  describe "::INT64_TYPE" do
+
+    it "returns 0x12" do
+      expect(Integer::INT64_TYPE).to eq(18.chr)
+    end
+  end
+
+  describe "#bson_int32?" do
+
+    context "when the integer is 32 bit" do
+
+      let(:integer) do
+        Integer::MAX_32BIT - 1
+      end
+
+      it "returns true" do
+        expect(integer).to be_bson_int32
+      end
+    end
+
+    context "when the integer is not 32 bit" do
+
+      let(:integer) do
+        Integer::MAX_32BIT + 1
+      end
+
+      it "returns false" do
+        expect(integer).to_not be_bson_int32
+      end
+    end
+  end
+
+  describe "#bson_int64?" do
+
+    context "when the integer is 64 bit" do
+
+      let(:integer) do
+        Integer::MAX_64BIT - 1
+      end
+
+      it "returns true" do
+        expect(integer).to be_bson_int64
+      end
+    end
+
+    context "when the integer is not 64 bit" do
+
+      let(:integer) do
+        Integer::MAX_64BIT + 1
+      end
+
+      it "returns false" do
+        expect(integer).to_not be_bson_int64
+      end
+    end
+  end
+
+  describe "#bson_type" do
+
+    context "when the integer is 32 bit" do
+
+      let(:integer) do
+        Integer::MAX_32BIT - 1
+      end
+
+      it "returns the INT32_TYPE" do
+        expect(integer.bson_type).to eq(Integer::INT32_TYPE)
+      end
+    end
+
+    context "when the integer is 64 bit" do
+
+      let(:integer) do
+        Integer::MAX_64BIT - 1
+      end
+
+      it "returns the INT64_TYPE" do
+        expect(integer.bson_type).to eq(Integer::INT64_TYPE)
+      end
+    end
+
+    context "when the integer is too large" do
+
+      let(:integer) do
+        Integer::MAX_64BIT + 1
+      end
+
+      pending "should we raise an error"
+    end
+
+    context "when the intger is too small" do
+
+      let(:integer) do
+        Integer::MAX_64BIT - 1
+      end
+
+      pending "should we raise an error"
+    end
+  end
+
+  describe "#to_bson" do
+
+    context "when the integer is 32 bit" do
+
+      let(:integer) do
+        Integer::MAX_32BIT - 1
+      end
+
+      let(:expected) do
+        [ integer ].pack(Integer::INT32_PACK)
+      end
+
+      it "returns the 32 bit raw bytes" do
+        expect(integer.to_bson).to eq(expected)
+      end
+    end
+
+    context "when the integer is 64 bit" do
+
+      let(:integer) do
+        Integer::MAX_64BIT - 1
+      end
+
+      let(:expected) do
+        [ integer ].pack(Integer::INT64_PACK)
+      end
+
+      it "returns the 64 bit raw bytes" do
+        expect(integer.to_bson).to eq(expected)
+      end
+    end
+
+    context "when the integer is too large" do
+
+      let(:integer) do
+        Integer::MAX_64BIT + 1
+      end
+
+      pending "should we raise an error"
+    end
+
+    context "when the intger is too small" do
+
+      let(:integer) do
+        Integer::MAX_64BIT - 1
+      end
+
+      pending "should we raise an error"
+    end
+  end
+end
