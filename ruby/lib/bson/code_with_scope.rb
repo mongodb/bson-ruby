ba3b64cabf862a9d21b2ba821d5951ab7d20ab3f
Durran Jordan <durran@gmail.com>
2013-03-02 12:42:42 +0100

    Add code with scope equality check

--- ruby/lib/bson/code_with_scope.rb
+++ ruby/lib/bson/code_with_scope.rb
@@ -14,6 +14,43 @@ module BSON
     # @since 2.0.0
     BSON_TYPE = 15.chr.freeze
 
+    # @!attribute javascript
+    #   @return [ String ] The javascript code.
+    #   @since 2.0.0
+    # @!attribute scope
+    #   @return [ Hash ] The variable scope.
+    #   @since 2.0.0
+    attr_reader :javascript, :scope
+
+    # Determine if this code with scope object is equal to another object.
+    #
+    # @example Check the code with scope equality.
+    #   code_with_scope == other
+    #
+    # @param [ Object ] other The object to compare against.
+    #
+    # @return [ true, false ] If the objects are equal.
+    #
+    # @since 2.0.0
+    def ==(other)
+      return false unless other.is_a?(CodeWithScope)
+      javascript == other.javascript && scope == other.scope
+    end
+
+    # Instantiate the new code with scope.
+    #
+    # @example Instantiate the code with scope.
+    #   BSON::CodeWithScope.new("this.value = name", name: "sid")
+    #
+    # @param [ String ] javascript The javascript code.
+    # @param [ Hash ] scope The variable scope.
+    #
+    # @since 2.0.0
+    def initialize(javascript, scope = {})
+      @javascript = javascript
+      @scope = scope
+    end
+
     # Encode the javascript code with scope.
     #
     # @example Encode the code with scope.
--- ruby/spec/bson/code_with_scope_spec.rb
+++ ruby/spec/bson/code_with_scope_spec.rb
@@ -2,6 +2,42 @@ require "spec_helper"
 
 describe BSON::CodeWithScope do
 
+  describe "#==" do
+
+    let(:code_with_scope) do
+      described_class.new("this.value == name;", :name => "test")
+    end
+
+    context "when the objects are equal" do
+
+      let(:other) do
+        described_class.new("this.value == name;", :name => "test")
+      end
+
+      it "returns true" do
+        expect(code_with_scope).to eq(other)
+      end
+    end
+
+    context "when the objects are not equal" do
+
+      let(:other) do
+        described_class.new("this.value == name;", :value => "test")
+      end
+
+      it "returns false" do
+        expect(code_with_scope).to_not eq(other)
+      end
+    end
+
+    context "when the other object is not a code_with_scope" do
+
+      it "returns false" do
+        expect(code_with_scope).to_not eq("test")
+      end
+    end
+  end
+
   describe "::BSON_TYPE" do
 
     it "returns 0x0F" do
@@ -12,7 +48,7 @@ describe BSON::CodeWithScope do
   describe "#bson_type" do
 
     let(:code_with_scope) do
-      described_class.new
+      described_class.new("this.value = name", :name => "test")
     end
 
     it "returns 0x0F" do
