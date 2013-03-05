96e2715d8df623a84361090b61dd6225d0e0be70
Tyler Brock <tyler.brock@gmail.com>
2013-03-04 21:47:28 -0500

    deserialize booleans

--- ruby/lib/bson/boolean.rb
+++ ruby/lib/bson/boolean.rb
@@ -14,6 +14,19 @@ module BSON
     # @since 2.0.0
     BSON_TYPE = 8.chr.force_encoding(BINARY).freeze
 
+    # Deserialize a boolean from BSON.
+    #
+    # @param [ BSON ] bson The encoded boolean.
+    #
+    # @return [ TrueClass, FalseClass ] The decoded boolean.
+    #
+    # @see http://bsonspec.org/#/specification
+    #
+    # @since 2.0.0
+    def from_bson(bson)
+      bson.readbyte == 1
+    end
+
     # Register this type when the module is loaded.
     #
     # @since 2.0.0
