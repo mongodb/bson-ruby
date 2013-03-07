979ed6f2d34fbd77c7d2abc0f3a2a07d050b0b75
Tyler Brock <tyler.brock@gmail.com>
2013-03-07 16:02:35 -0500

    add additional shared examples and wrap in BSON module

--- ruby/spec/support/shared_examples.rb
+++ ruby/spec/support/shared_examples.rb
@@ -1,11 +1,31 @@
 # encoding: utf-8
-shared_examples_for "a binary encoded string" do
 
-  let(:binary_encoding) do
-    Encoding.find(BSON::BINARY)
+module BSON
+  shared_examples_for "a binary encoded string" do
+    let(:binary_encoding) do
+      Encoding.find(BSON::BINARY)
+    end
+
+    it "returns the string with binary encoding" do
+      expect(encoded.encoding).to eq(binary_encoding)
+    end
   end
 
-  it "returns the string with binary encoding" do
-    expect(encoded.encoding).to eq(binary_encoding)
+  shared_examples_for 'a bson element' do
+    subject { (defined? obj) ? obj : described_class.new  }
+    its(:bson_type) { should eq(type) }
+  end
+
+  shared_examples_for 'a serializable bson element' do
+    it 'serializes to bson' do
+      obj.to_bson.should == bson
+    end
+  end
+
+  shared_examples_for 'a deserializable bson element' do
+    it 'deserializes from bson' do
+      io = StringIO.new(bson)
+      described_class.from_bson(io).should == obj
+    end
   end
 end
