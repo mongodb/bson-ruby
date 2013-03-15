f97bd4a2bceb36f904623de8f323a68ff7ca88e4
Durran Jordan <durran@gmail.com>
2013-03-15 13:35:10 +0100

    Modifying boolean specs

--- ruby/spec/bson/boolean_spec.rb
+++ ruby/spec/bson/boolean_spec.rb
@@ -2,19 +2,34 @@
 require "spec_helper"
 
 describe BSON::Boolean do
-  let(:type) { 8.chr }
 
-  it_behaves_like "a bson element"
+  describe "::BSON_TYPE" do
 
-  context "true class" do
-    let(:obj)  { true }
-    let(:bson) { 1.chr }
-    it_behaves_like "a deserializable bson element"
+    it "returns 8" do
+      expect(BSON::Boolean::BSON_TYPE).to eq(8.chr)
+    end
   end
 
-  context "false class" do
-    let(:obj)  { false }
-    let(:bson) { 0.chr }
-    it_behaves_like "a deserializable bson element"
+  describe "#from_bson" do
+
+    let(:type) { 8.chr }
+
+    it_behaves_like "a bson element"
+
+    context "when the boolean is true" do
+
+      let(:obj)  { true }
+      let(:bson) { 1.chr }
+
+      it_behaves_like "a deserializable bson element"
+    end
+
+    context "when the boolean is false" do
+
+      let(:obj)  { false }
+      let(:bson) { 0.chr }
+
+      it_behaves_like "a deserializable bson element"
+    end
   end
 end
