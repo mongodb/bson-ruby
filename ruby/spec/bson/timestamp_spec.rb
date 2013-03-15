7a70bb32e58dfc282dbd40675cfc3dc281ae2f43
Durran Jordan <durran@gmail.com>
2013-03-15 14:16:58 +0100

    Fix timestamp spec

--- ruby/spec/bson/timestamp_spec.rb
+++ ruby/spec/bson/timestamp_spec.rb
@@ -3,6 +3,38 @@ require "spec_helper"
 
 describe BSON::Timestamp do
 
+  describe "#==" do
+
+    let(:timestamp) do
+      described_class.new(1, 10)
+    end
+
+    context "when the objects are equal" do
+
+      let(:other) { described_class.new(1, 10) }
+
+      it "returns true" do
+        expect(timestamp).to eq(other)
+      end
+    end
+
+    context "when the objects are not equal" do
+
+      let(:other) { described_class.new(1, 15) }
+
+      it "returns false" do
+        expect(timestamp).to_not eq(other)
+      end
+    end
+
+    context "when the other object is not a timestamp" do
+
+      it "returns false" do
+        expect(timestamp).to_not eq("test")
+      end
+    end
+  end
+
   describe "#as_json" do
 
     let(:object) do
@@ -16,35 +48,14 @@ describe BSON::Timestamp do
     it_behaves_like "a JSON serializable object"
   end
 
-  let(:type) { 17.chr }
-  let(:obj)  { described_class.new(1, 10) }
-  let(:bson) { [ 10, 1 ].pack("l2") }
+  describe "#to_bson/#from_bson" do
 
-  it_behaves_like "a bson element"
-  it_behaves_like "a serializable bson element"
-  it_behaves_like "a deserializable bson element"
+    let(:type) { 17.chr }
+    let(:obj)  { described_class.new(1, 10) }
+    let(:bson) { [ 10, 1 ].pack("l2") }
 
-  describe "#==" do
-    context "when the objects are equal" do
-      let(:other) { described_class.new(1, 10) }
-
-      it "returns true" do
-        expect(obj).to eq(other)
-      end
-    end
-
-    context "when the objects are not equal" do
-      let(:other) { described_class.new(1, 15) }
-
-      it "returns false" do
-        expect(obj).to_not eq(other)
-      end
-    end
-
-    context "when the other object is not a timestamp" do
-      it "returns false" do
-        expect(obj).to_not eq("test")
-      end
-    end
+    it_behaves_like "a bson element"
+    it_behaves_like "a serializable bson element"
+    it_behaves_like "a deserializable bson element"
   end
 end
