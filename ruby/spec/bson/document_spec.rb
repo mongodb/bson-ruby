5ff485f80fe3ff5aba05da4bb4a311f549460648
Durran Jordan <durran@gmail.com>
2013-03-15 13:01:37 +0100

    Remove the false bson document tests

--- ruby/spec/bson/document_spec.rb
+++ ruby/spec/bson/document_spec.rb
@@ -3,16 +3,4 @@ require "spec_helper"
 
 describe BSON::Document do
 
-  let(:type) { 3.chr }
-  let(:obj)  { { :key => "value" } }
-  let(:bson) {
-    "#{20.to_bson}#{String::BSON_TYPE}key#{BSON::NULL_BYTE}" +
-    "#{6.to_bson}value#{BSON::NULL_BYTE}#{BSON::NULL_BYTE}"
-  }
-
-  it_behaves_like "a bson element"
-  it_behaves_like "a serializable bson element"
-  pending do
-    it_behaves_like "a deserializable bson element"
-  end
 end
