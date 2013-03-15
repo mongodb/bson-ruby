966b673a56d83f965ff97365569ffa4b1b8596a4
Durran Jordan <durran@gmail.com>
2013-03-15 14:08:49 +0100

    Reformat nil spec for consistency

--- ruby/spec/bson/nil_class_spec.rb
+++ ruby/spec/bson/nil_class_spec.rb
@@ -2,11 +2,15 @@
 require "spec_helper"
 
 describe NilClass do
-  let(:type) { 10.chr }
-  let(:obj)  { nil }
-  let(:bson) { BSON::NO_VALUE }
 
-  it_behaves_like "a bson element"
-  it_behaves_like "a serializable bson element"
-  it_behaves_like "a deserializable bson element"
+  describe "#to_bson/#from_bson" do
+
+    let(:type) { 10.chr }
+    let(:obj)  { nil }
+    let(:bson) { BSON::NO_VALUE }
+
+    it_behaves_like "a bson element"
+    it_behaves_like "a serializable bson element"
+    it_behaves_like "a deserializable bson element"
+  end
 end
