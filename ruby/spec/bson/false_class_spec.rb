fbb2508e2e7d4ce700674e8f253300879fe43c9d
Durran Jordan <durran@gmail.com>
2013-03-15 14:02:51 +0100

    Reformat false spec

--- ruby/spec/bson/false_class_spec.rb
+++ ruby/spec/bson/false_class_spec.rb
@@ -2,8 +2,12 @@
 require "spec_helper"
 
 describe FalseClass do
-  let(:obj)  { false }
-  let(:bson) { 0.chr }
 
-  it_behaves_like "a serializable bson element"
+  describe "#to_bson" do
+
+    let(:obj)  { false }
+    let(:bson) { 0.chr }
+
+    it_behaves_like "a serializable bson element"
+  end
 end
