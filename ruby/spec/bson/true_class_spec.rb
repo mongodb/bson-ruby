2d8c8768d2adf90746492d2f899021a29c2e4001
Durran Jordan <durran@gmail.com>
2013-03-15 14:17:35 +0100

    Reformat true spec

--- ruby/spec/bson/true_class_spec.rb
+++ ruby/spec/bson/true_class_spec.rb
@@ -2,8 +2,12 @@
 require "spec_helper"
 
 describe TrueClass do
-  let(:obj)  { true }
-  let(:bson) { 1.chr }
 
-  it_behaves_like "a serializable bson element"
+  describe "#to_bson" do
+
+    let(:obj)  { true }
+    let(:bson) { 1.chr }
+
+    it_behaves_like "a serializable bson element"
+  end
 end
