7d8d7677429472faac262da675b1a115e86cc238
Durran Jordan <durran@gmail.com>
2013-03-15 14:14:15 +0100

    Reformat symbol spec

--- ruby/spec/bson/symbol_spec.rb
+++ ruby/spec/bson/symbol_spec.rb
@@ -2,11 +2,15 @@
 require "spec_helper"
 
 describe Symbol do
-  let(:type) { 14.chr }
-  let(:obj)  { :test }
-  let(:bson) { "#{5.to_bson}test#{BSON::NULL_BYTE}" }
 
-  it_behaves_like "a bson element"
-  it_behaves_like "a serializable bson element"
-  it_behaves_like "a deserializable bson element"
+  describe "#to_bson/#from_bson" do
+
+    let(:type) { 14.chr }
+    let(:obj)  { :test }
+    let(:bson) { "#{5.to_bson}test#{BSON::NULL_BYTE}" }
+
+    it_behaves_like "a bson element"
+    it_behaves_like "a serializable bson element"
+    it_behaves_like "a deserializable bson element"
+  end
 end
