01236bdd7d7b35b7999418f09ac95d2050078176
Durran Jordan <durran@gmail.com>
2013-03-15 14:09:59 +0100

    Reformat regexp spec

--- ruby/spec/bson/regexp_spec.rb
+++ ruby/spec/bson/regexp_spec.rb
@@ -18,45 +18,59 @@ describe Regexp do
     it_behaves_like "a JSON serializable object"
   end
 
-  let(:type) { 11.chr }
-  let(:obj)  { /test/ }
+  describe "#to_bson/#from_bson" do
 
-  it_behaves_like "a bson element"
+    let(:type) { 11.chr }
+    let(:obj)  { /test/ }
 
-  context "when the regexp has no options" do
-    let(:obj)  { /\d+/ }
-    let(:bson) { "#{obj.source}#{BSON::NULL_BYTE}#{BSON::NULL_BYTE}" }
-    it_behaves_like "a serializable bson element"
-    it_behaves_like "a deserializable bson element"
-  end
+    it_behaves_like "a bson element"
 
-  context "when the regexp has options" do
-    context "when ignoring case" do
-      let(:obj)  { /\W+/i }
-      let(:bson) { "#{obj.source}#{BSON::NULL_BYTE}i#{BSON::NULL_BYTE}" }
+    context "when the regexp has no options" do
+
+      let(:obj)  { /\d+/ }
+      let(:bson) { "#{obj.source}#{BSON::NULL_BYTE}#{BSON::NULL_BYTE}" }
+
       it_behaves_like "a serializable bson element"
       it_behaves_like "a deserializable bson element"
     end
 
-    context "when matching multiline" do
-      let(:obj)  { /\W+/m }
-      let(:bson) { "#{obj.source}#{BSON::NULL_BYTE}ms#{BSON::NULL_BYTE}" }
-      it_behaves_like "a serializable bson element"
-      it_behaves_like "a deserializable bson element"
-    end
+    context "when the regexp has options" do
 
-    context "when matching extended" do
-      let(:obj)  { /\W+/x }
-      let(:bson) { "#{obj.source}#{BSON::NULL_BYTE}x#{BSON::NULL_BYTE}" }
-      it_behaves_like "a serializable bson element"
-      it_behaves_like "a deserializable bson element"
-    end
+      context "when ignoring case" do
 
-    context "when all options are present" do
-      let(:obj)  { /\W+/xim }
-      let(:bson) { "#{obj.source}#{BSON::NULL_BYTE}imsx#{BSON::NULL_BYTE}" }
-      it_behaves_like "a serializable bson element"
-      it_behaves_like "a deserializable bson element"
+        let(:obj)  { /\W+/i }
+        let(:bson) { "#{obj.source}#{BSON::NULL_BYTE}i#{BSON::NULL_BYTE}" }
+
+        it_behaves_like "a serializable bson element"
+        it_behaves_like "a deserializable bson element"
+      end
+
+      context "when matching multiline" do
+
+        let(:obj)  { /\W+/m }
+        let(:bson) { "#{obj.source}#{BSON::NULL_BYTE}ms#{BSON::NULL_BYTE}" }
+
+        it_behaves_like "a serializable bson element"
+        it_behaves_like "a deserializable bson element"
+      end
+
+      context "when matching extended" do
+
+        let(:obj)  { /\W+/x }
+        let(:bson) { "#{obj.source}#{BSON::NULL_BYTE}x#{BSON::NULL_BYTE}" }
+
+        it_behaves_like "a serializable bson element"
+        it_behaves_like "a deserializable bson element"
+      end
+
+      context "when all options are present" do
+
+        let(:obj)  { /\W+/xim }
+        let(:bson) { "#{obj.source}#{BSON::NULL_BYTE}imsx#{BSON::NULL_BYTE}" }
+
+        it_behaves_like "a serializable bson element"
+        it_behaves_like "a deserializable bson element"
+      end
     end
   end
 end
