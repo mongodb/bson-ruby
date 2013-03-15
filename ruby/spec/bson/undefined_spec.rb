9f750612b0798c024d7f0c05ba51566a460c11f4
Durran Jordan <durran@gmail.com>
2013-03-15 14:20:15 +0100

    Reformat undefined spec

--- ruby/spec/bson/undefined_spec.rb
+++ ruby/spec/bson/undefined_spec.rb
@@ -2,11 +2,15 @@
 require "spec_helper"
 
 describe BSON::Undefined do
-  let(:type) { 6.chr }
-  let(:obj)  { described_class.new }
-  let(:bson) { BSON::NO_VALUE }
 
-  it_behaves_like "a bson element"
-  it_behaves_like "a serializable bson element"
-  it_behaves_like "a deserializable bson element"
+  describe "#to_bson/#from_bson" do
+
+    let(:type) { 6.chr }
+    let(:obj)  { described_class.new }
+    let(:bson) { BSON::NO_VALUE }
+
+    it_behaves_like "a bson element"
+    it_behaves_like "a serializable bson element"
+    it_behaves_like "a deserializable bson element"
+  end
 end
