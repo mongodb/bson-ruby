eafd6f957e1875db02910d053254b58ff853a2cb
Durran Jordan <durran@gmail.com>
2013-03-15 14:06:17 +0100

    Reformat integer spec for consistency

--- ruby/spec/bson/integer_spec.rb
+++ ruby/spec/bson/integer_spec.rb
@@ -3,39 +3,46 @@ require "spec_helper"
 
 describe Integer do
 
-  context "when the integer is 32 bit" do
-    it_behaves_like "a serializable bson element" do
+  describe "#to_bson" do
+
+    context "when the integer is 32 bit" do
+
       let(:type) { 16.chr }
       let(:obj)  { Integer::MAX_32BIT - 1 }
       let(:bson) { [ obj ].pack(BSON::Int32::PACK) }
-    end
-  end
 
-  context "when the integer is 64 bit" do
-    it_behaves_like "a serializable bson element" do
+      it_behaves_like "a serializable bson element"
+    end
+
+    context "when the integer is 64 bit" do
+
       let(:type) { 18.chr }
       let(:obj)  { Integer::MAX_64BIT - 1 }
       let(:bson) { [ obj ].pack(BSON::Int64::PACK) }
+
+      it_behaves_like "a serializable bson element"
     end
-  end
 
-  context "when the integer is too large" do
-    let(:integer) { Integer::MAX_64BIT + 1 }
+    context "when the integer is too large" do
 
-    it "raises an out of range error" do
-      expect {
-        integer.to_bson
-      }.to raise_error(BSON::Integer::OutOfRange)
+      let(:integer) { Integer::MAX_64BIT + 1 }
+
+      it "raises an out of range error" do
+        expect {
+          integer.to_bson
+        }.to raise_error(BSON::Integer::OutOfRange)
+      end
     end
-  end
 
-  context "when the intger is too small" do
-    let(:integer) { Integer::MIN_64BIT - 1 }
+    context "when the intger is too small" do
 
-    it "raises an out of range error" do
-      expect {
-        integer.to_bson
-      }.to raise_error(BSON::Integer::OutOfRange)
+      let(:integer) { Integer::MIN_64BIT - 1 }
+
+      it "raises an out of range error" do
+        expect {
+          integer.to_bson
+        }.to raise_error(BSON::Integer::OutOfRange)
+      end
     end
   end
 end
