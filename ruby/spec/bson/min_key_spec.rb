b27015e0266c6d3126422c52cc3702ac01e58d57
Durran Jordan <durran@gmail.com>
2013-03-15 14:08:15 +0100

    Reformat min key spec for consistency

--- ruby/spec/bson/min_key_spec.rb
+++ ruby/spec/bson/min_key_spec.rb
@@ -16,16 +16,10 @@ describe BSON::MinKey do
     it_behaves_like "a JSON serializable object"
   end
 
-  let(:type) { 255.chr }
-  let(:obj)  { described_class.new }
-  let(:bson) { BSON::NO_VALUE }
-
-  it_behaves_like "a bson element"
-  it_behaves_like "a serializable bson element"
-  it_behaves_like "a deserializable bson element"
-
   describe "#==" do
+
     context "when the objects are equal" do
+
       let(:other) { described_class.new }
 
       it "returns true" do
@@ -34,6 +28,7 @@ describe BSON::MinKey do
     end
 
     context "when the other object is not a max_key" do
+
       it "returns false" do
         expect(subject).to_not eq("test")
       end
@@ -41,14 +36,27 @@ describe BSON::MinKey do
   end
 
   describe "#>" do
+
     it "always returns false" do
       expect(subject > Integer::MAX_64BIT).to be_false
     end
   end
 
   describe "#<" do
+
     it "always returns true" do
       expect(subject < Integer::MAX_64BIT).to be_true
     end
+  end
+
+  describe "#to_bson/#from_bson" do
+
+    let(:type) { 255.chr }
+    let(:obj)  { described_class.new }
+    let(:bson) { BSON::NO_VALUE }
+
+    it_behaves_like "a bson element"
+    it_behaves_like "a serializable bson element"
+    it_behaves_like "a deserializable bson element"
   end
 end
