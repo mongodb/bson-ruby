5dfe4efcf0faed11a083c1a4658d0fb94cf8d16f
Durran Jordan <durran@gmail.com>
2013-03-17 15:34:23 +0100

    Make BSON::Document behave as a hash for 1.9

--- ruby/spec/bson/document_spec.rb
+++ ruby/spec/bson/document_spec.rb
@@ -3,4 +3,45 @@ require "spec_helper"
 
 describe BSON::Document do
 
+  describe "#to_bson/#from_bson" do
+
+    let(:type) { 3.chr }
+
+    it_behaves_like "a bson element"
+
+    context "when the hash is a single level" do
+
+      let(:obj) do
+        described_class["key" => "value"]
+      end
+
+      let(:bson) do
+        "#{20.to_bson}#{String::BSON_TYPE}key#{BSON::NULL_BYTE}" +
+        "#{6.to_bson}value#{BSON::NULL_BYTE}#{BSON::NULL_BYTE}"
+      end
+
+      it_behaves_like "a serializable bson element"
+      pending do
+        it_behaves_like "a deserializable bson element"
+      end
+    end
+
+    context "when the hash is embedded" do
+
+      let(:obj) do
+        described_class["field" => { "key" => "value" }]
+      end
+
+      let(:bson) do
+        "#{32.to_bson}#{Hash::BSON_TYPE}field#{BSON::NULL_BYTE}" +
+        "#{20.to_bson}#{String::BSON_TYPE}key#{BSON::NULL_BYTE}" +
+        "#{6.to_bson}value#{BSON::NULL_BYTE}#{BSON::NULL_BYTE}#{BSON::NULL_BYTE}"
+      end
+
+      it_behaves_like "a serializable bson element"
+      pending do
+        it_behaves_like "a deserializable bson element"
+      end
+    end
+  end
 end
--- ruby/spec/bson/hash_spec.rb
+++ ruby/spec/bson/hash_spec.rb
@@ -2,34 +2,46 @@
 require "spec_helper"
 
 describe Hash do
-  let(:type) { 3.chr }
 
-  it_behaves_like "a bson element"
+  describe "#to_bson/#from_bson" do
 
-  context "when the hash is a single level" do
-    let(:obj)  { { "key" => "value" } }
-    let(:bson) {
-      "#{20.to_bson}#{String::BSON_TYPE}key#{BSON::NULL_BYTE}" +
-      "#{6.to_bson}value#{BSON::NULL_BYTE}#{BSON::NULL_BYTE}"
-    }
+    let(:type) { 3.chr }
 
-    it_behaves_like "a serializable bson element"
-    pending do
-      it_behaves_like "a deserializable bson element"
+    it_behaves_like "a bson element"
+
+    context "when the hash is a single level" do
+
+      let(:obj) do
+        { "key" => "value" }
+      end
+
+      let(:bson) do
+        "#{20.to_bson}#{String::BSON_TYPE}key#{BSON::NULL_BYTE}" +
+        "#{6.to_bson}value#{BSON::NULL_BYTE}#{BSON::NULL_BYTE}"
+      end
+
+      it_behaves_like "a serializable bson element"
+      pending do
+        it_behaves_like "a deserializable bson element"
+      end
     end
-  end
 
-  context "when the hash is embedded" do
-    let(:obj)  { { "field" => { "key" => "value" } } }
-    let(:bson) {
-      "#{32.to_bson}#{Hash::BSON_TYPE}field#{BSON::NULL_BYTE}" +
-      "#{20.to_bson}#{String::BSON_TYPE}key#{BSON::NULL_BYTE}" +
-      "#{6.to_bson}value#{BSON::NULL_BYTE}#{BSON::NULL_BYTE}#{BSON::NULL_BYTE}"
-    }
+    context "when the hash is embedded" do
 
-    it_behaves_like "a serializable bson element"
-    pending do
-      it_behaves_like "a deserializable bson element"
+      let(:obj) do
+        { "field" => { "key" => "value" }}
+      end
+
+      let(:bson) do
+        "#{32.to_bson}#{Hash::BSON_TYPE}field#{BSON::NULL_BYTE}" +
+        "#{20.to_bson}#{String::BSON_TYPE}key#{BSON::NULL_BYTE}" +
+        "#{6.to_bson}value#{BSON::NULL_BYTE}#{BSON::NULL_BYTE}#{BSON::NULL_BYTE}"
+      end
+
+      it_behaves_like "a serializable bson element"
+      pending do
+        it_behaves_like "a deserializable bson element"
+      end
     end
   end
 end
