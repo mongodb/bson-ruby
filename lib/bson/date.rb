ad78cd261323c4eb34cf51605f3e06a974443a69
Durran Jordan <durran@gmail.com>
2014-01-09 10:34:36 +0100

    Serialize Date objects to BSON:
    For dates, we create a UTC time at midnight for the Date, and then
    serialize the time.
    
    There is no deserialization into Dates, since BSON only has a Time type,
    but is useful for queries where a user is working with Date objects and
    doesn't want to manually convert to a Time themselves at each instance.

---
+++ lib/bson/date.rb
@@ -0,0 +1,44 @@
+# Copyright (C) 2009-2013 MongoDB Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+module BSON
+
+  # Injects behaviour for encoding date values to raw bytes as specified by
+  # the BSON spec for time.
+  #
+  # @see http://bsonspec.org/#/specification
+  #
+  # @since 2.1.0
+  module Date
+
+    # Get the date as encoded BSON.
+    #
+    # @example Get the date as encoded BSON.
+    #   Date.new(2012, 1, 1).to_bson
+    #
+    # @return [ String ] The encoded string.
+    #
+    # @see http://bsonspec.org/#/specification
+    #
+    # @since 2.1.0
+    def to_bson(encoded = ''.force_encoding(BINARY))
+      ::Time.utc(year, month, day).to_bson(encoded)
+    end
+  end
+
+  # Enrich the core Date class with this module.
+  #
+  # @since 2.1.0
+  ::Date.send(:include, Date)
+end
---
+++ spec/bson/date_spec.rb
@@ -0,0 +1,39 @@
+# Copyright (C) 2009-2013 MongoDB Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "spec_helper"
+
+describe Date do
+
+  describe "#to_bson" do
+
+    context "when the date is post epoch" do
+
+      let(:obj)  { Date.new(2012, 1, 1) }
+      let(:time) { Time.utc(2012, 1, 1) }
+      let(:bson) { [ (time.to_f * 1000).to_i ].pack(BSON::Int64::PACK) }
+
+      it_behaves_like "a serializable bson element"
+    end
+
+    context "when the date is pre epoch" do
+
+      let(:obj)  { Date.new(1969, 1, 1) }
+      let(:time) { Time.utc(1969, 1, 1) }
+      let(:bson) { [ (time.to_f * 1000).to_i ].pack(BSON::Int64::PACK) }
+
+      it_behaves_like "a serializable bson element"
+    end
+  end
+end
