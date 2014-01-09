e1a0bf027113e4aed93d8bae3231262df971b494
Durran Jordan <durran@gmail.com>
2014-01-09 11:23:40 +0100

    Serialize DateTime objects to BSON.
    Simply converts the DateTime to a Time and calls to_bson. There is no
    explicity conversion to UTC as it is up to the application itself to
    deal with the time zone/utc conversions properly.

---
+++ lib/bson/date_time.rb
@@ -0,0 +1,44 @@
+# Copyright (C) 2009-2013 MongoDB Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+module BSON
+
+  # Injects behaviour for encoding date time values to raw bytes as specified by
+  # the BSON spec for time.
+  #
+  # @see http://bsonspec.org/#/specification
+  #
+  # @since 2.1.0
+  module DateTime
+
+    # Get the date time as encoded BSON.
+    #
+    # @example Get the date time as encoded BSON.
+    #   DateTime.new(2012, 1, 1, 0, 0, 0).to_bson
+    #
+    # @return [ String ] The encoded string.
+    #
+    # @see http://bsonspec.org/#/specification
+    #
+    # @since 2.1.0
+    def to_bson(encoded = ''.force_encoding(BINARY))
+      to_time.to_bson(encoded)
+    end
+  end
+
+  # Enrich the core DateTime class with this module.
+  #
+  # @since 2.1.0
+  ::DateTime.send(:include, DateTime)
+end
