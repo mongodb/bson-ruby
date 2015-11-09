6fbd58794a56457fa8a7463eb70c56e004670d3c
Durran Jordan <durran@gmail.com>
2015-11-09 13:55:10 -0500

    Use slow swap functions on solaris

--- ext/bson/native-endian.h
+++ ext/bson/native-endian.h
@@ -43,10 +43,7 @@
 # endif
 #endif
 
-#if defined(__sun)
-# define BSON_UINT32_SWAP_LE_BE(v) BSWAP_32((uint32_t)v)
-# define BSON_UINT64_SWAP_LE_BE(v) BSWAP_64((uint64_t)v)
-#elif defined(__clang__) && defined(__clang_major__) && defined(__clang_minor__) && \
+#if defined(__clang__) && defined(__clang_major__) && defined(__clang_minor__) && \
   (__clang_major__ >= 3) && (__clang_minor__ >= 1)
 # if __has_builtin(__builtin_bswap32)
 #  define BSON_UINT32_SWAP_LE_BE(v) __builtin_bswap32(v)
