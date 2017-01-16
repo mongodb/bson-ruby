8a4f9c82cbecd15abbe6a162b92813216f9c0384
Emily <emily@mongodb.com>
2017-01-16 15:15:37 +0100

    Use driver evergreen tools to test on Evergreen CI

---
+++ .evergreen/run-tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -o xtrace   # Write all commands first to stderr
+set -o errexit  # Exit the script with error if any of the commands fail
+
+# Supported/used environment variables:
+#       RVM_RUBY      Define the Ruby version to test with, using its RVM identifier.
+#                     For example: "ruby-2.3" or "jruby-9.1"
+
+RVM_RUBY=${RVM_RUBY:-}
+
+source ~/.rvm/scripts/rvm
+rvm use $RVM_RUBY
+
+echo "Installing all gem dependencies"
+bundle install
+bundle exec rake clean
+
+echo "Running specs"
+bundle exec rake spec
