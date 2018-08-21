ef18dddce818b11be53213e4d40dd7d8f8112433
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2018-08-21 11:30:46 -0400

    Test on Jruby (#98)
    Test on jruby on Evergreen, shorten Travis matrix

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -10,8 +10,17 @@ set -o errexit  # Exit the script with error if any of the commands fail
 RVM_RUBY=${RVM_RUBY:-}
 
 # Necessary for jruby
-export JAVACMD=/opt/java/jdk8/bin/java
-export PATH=$PATH:/opt/java/jdk8/bin
+# Use toolchain java if it exists
+if [ -f /opt/java/jdk8/bin/java ]; then
+  export JAVACMD=/opt/java/jdk8/bin/java
+  export PATH=$PATH:/opt/java/jdk8/bin
+fi
+  
+# ppc64le has it in a different place
+if test -z "$JAVACMD" && [ -f /usr/lib/jvm/java-1.8.0/bin/java ]; then
+  export JAVACMD=/usr/lib/jvm/java-1.8.0/bin/java
+  export PATH=$PATH:/usr/lib/jvm/java-1.8.0/bin
+fi
 
 if [ "$RVM_RUBY" == "ruby-head" ]; then
   # 12.04, 14.04 and 16.04 are good
@@ -23,12 +32,23 @@ if [ "$RVM_RUBY" == "ruby-head" ]; then
   
   # With rvm we reinstall ruby-head every run: rvm reinstall $RVM_RUBY
 else
-  . ~/.rvm/scripts/rvm
-
-  # Don't errexit because this may call scripts which error
-  set +o errexit
-  rvm use $RVM_RUBY
-  set -o errexit
+  # Testing toolchain
+  #toolchain_url=https://s3.amazonaws.com//mciuploads/mongo-ruby-toolchain/rhel70/07f2c6cf44624721cfc614547de3b2db8fb29919/mongo_ruby_driver_toolchain_rhel70_07f2c6cf44624721cfc614547de3b2db8fb29919_18_07_27_19_35_52.tar.gz
+  #curl -fL $toolchain_url |tar zxf -
+  #export PATH=`pwd`/rubies/$RVM_RUBY/bin:$PATH
+  
+  # Normal operation
+  if ! test -d $HOME/.rubies/$RVM_RUBY/bin; then
+    echo "Ruby directory does not exist: $HOME/.rubies/$RVM_RUBY/bin" 1>&2
+    echo "Contents of /opt:" 1>&2
+    ls -l /opt 1>&2 || true
+    echo ".rubies symlink:" 1>&2
+    ls -ld $HOME/.rubies 1>&2 || true
+    echo "Our rubies:" 1>&2
+    ls -l $HOME/.rubies 1>&2 || true
+    exit 2
+  fi
+  export PATH=$HOME/.rubies/$RVM_RUBY/bin:$PATH
 
   # Ensure we're using the right ruby
   python - <<EOH
@@ -38,8 +58,8 @@ assert(ruby in "`ruby --version`")
 assert(version in "`ruby --version`")
 EOH
 
-  echo 'updating rubygems'
-  gem update --system
+  #echo 'updating rubygems'
+  #gem update --system
 
   # ruby-head comes with bundler and gem complains
   # because installing bundler would overwrite the bundler binary
