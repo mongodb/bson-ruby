bcf9ba1ae9b87071242df479adbc59cec4a24b55
Oleg Pudeyev <oleg@bsdpower.com>
2019-01-18 11:27:47 -0500

    Also cover passing block when the key exists

--- spec/bson/document_spec.rb
+++ spec/bson/document_spec.rb
@@ -77,14 +77,14 @@ describe BSON::Document do
         end.to raise_exception(KeyError)
       end
 
-      context "and default value was provided" do
+      context "and default value is provided" do
 
         it "returns default value" do
           expect(document.fetch(:non_existent_key, false)).to eq(false)
         end
       end
 
-      context "and block was passed" do
+      context "and block is passed" do
 
         it "returns result of the block" do
           expect(document.fetch(:non_existent_key, &:to_s))
@@ -99,6 +99,13 @@ describe BSON::Document do
 
         it "returns the value" do
           expect(document.fetch(:key, "other")).to eq("value")
+        end
+      end
+
+      context "and block is passed" do
+
+        it "returns the value" do
+          expect(document.fetch(:key, &:to_s)).to eq("value")
         end
       end
     end
