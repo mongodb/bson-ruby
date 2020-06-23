0e23364bef9f3caafb36a15907d017b5397eb8b0
Emily Giurleo <e.m.giurleo@gmail.com>
2020-06-23 15:35:56 -0400

    add compaction mode for specs

--- spec/support/spec_config.rb
+++ spec/support/spec_config.rb
@@ -3,7 +3,23 @@ require 'singleton'
 class SpecConfig
   include Singleton
 
+  COMPACTION_CHANCE = 0.001
+
   def active_support?
     %w(1 true yes).include?(ENV['WITH_ACTIVE_SUPPORT'])
+  end
+
+  def compact?
+    %w(1 true yes).include?(ENV['COMPACT'])
+  end
+end
+
+RSpec.configure do |config|
+  if SpecConfig.instance.compact?
+    config.after do
+      if rand < SpecConfig::COMPACTION_CHANCE
+        GC.compact
+      end
+    end
   end
 end
