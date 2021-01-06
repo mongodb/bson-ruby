0726364fc0132c3a0c63eef58324b795565247af
Oleg Pudeyev <oleg@bsdpower.com>
2021-01-05 23:15:45 -0500

    test activesupport 5.2, 6.0 and 6.1 separately

--- spec/support/spec_config.rb
+++ spec/support/spec_config.rb
@@ -6,7 +6,8 @@ class SpecConfig
   COMPACTION_CHANCE = 0.001
 
   def active_support?
-    %w(1 true yes).include?(ENV['WITH_ACTIVE_SUPPORT'])
+    %w(1 true yes).include?(ENV['WITH_ACTIVE_SUPPORT']) ||
+      ENV['WITH_ACTIVE_SUPPORT'] =~ /[0-9]/ && ENV['WITH_ACTIVE_SUPPORT'] != '0'
   end
 
   def compact?
