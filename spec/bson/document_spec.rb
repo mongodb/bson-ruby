ea78135166ecbe3342c0a3dca5764d0b1414f478
Oleg Pudeyev <oleg@bsdpower.com>
2021-01-05 23:28:32 -0500

    #except is always defined

--- spec/bson/document_spec.rb
+++ spec/bson/document_spec.rb
@@ -229,24 +229,22 @@ describe BSON::Document do
     end
   end
 
-  if described_class.instance_methods.include?(:except)
-    describe "#except" do
-      let(:document) do
-        described_class.new("key1" => "value1", key2: "value2")
+  describe "#except" do
+    let(:document) do
+      described_class.new("key1" => "value1", key2: "value2")
+    end
+
+    context "when provided string keys" do
+
+      it "returns the partial document" do
+        expect(document.except("key1")).to contain_exactly(['key2', 'value2'])
       end
+    end
 
-      context "when provided string keys" do
+    context "when provided symbol keys" do
 
-        it "returns the partial document" do
-          expect(document.except("key1")).to contain_exactly(['key2', 'value2'])
-        end
-      end
-
-      context "when provided symbol keys" do
-
-        it "returns the partial document" do
-          expect(document.except(:key1)).to contain_exactly(['key2', 'value2'])
-        end
+      it "returns the partial document" do
+        expect(document.except(:key1)).to contain_exactly(['key2', 'value2'])
       end
     end
   end
