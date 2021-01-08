ad1e57760b02ca6be15220ef02b3746c7fd8d864
Alex Bevilacqua <alex@alexbevi.com>
2021-01-08 06:56:35 -0500

    RUBY-2438: Add permanent start of string and end of string anchors to uuid regex

--- lib/bson/binary.rb
+++ lib/bson/binary.rb
@@ -208,7 +208,7 @@ module BSON
           raise ArgumentError, "Binary of type :uuid can only be stringified to :standard representation, requested: #{representation.inspect}"
         end
 
-        data.split('').map { |n| '%02x' % n.ord }.join.sub(/(.{8})(.{4})(.{4})(.{4})(.{12})/, '\1-\2-\3-\4-\5')
+        data.split('').map { |n| '%02x' % n.ord }.join.sub(/\A(.{8})(.{4})(.{4})(.{4})(.{12})\z/, '\1-\2-\3-\4-\5')
       when :uuid_old
         if representation.nil?
           raise ArgumentError, 'Representation must be specified for BSON::Binary objects of type :uuid_old'
