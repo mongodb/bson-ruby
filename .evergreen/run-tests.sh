9c0ccf19d6a3c6c53742e8da17e1df7de0d97cd5
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2021-01-20 15:34:04 -0500

    RUBY-2488 Use shared tooling  (#232)
    * RUBY-2488 Use shared tooling
    
    * Need to initialize submodules
    
    Co-authored-by: Oleg Pudeyev <oleg@bsdpower.com>

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -7,11 +7,13 @@ set -o errexit  # Exit the script with error if any of the commands fail
 #       RVM_RUBY      Define the Ruby version to test with, using its RVM identifier.
 #                     For example: "ruby-2.3" or "jruby-9.2"
 
+. `dirname "$0"`/../spec/shared/shlib/distro.sh
+. `dirname "$0"`/../spec/shared/shlib/set_env.sh
 . `dirname "$0"`/functions.sh
 
 set_env_vars
 
-setup_ruby
+set_env_ruby
 
 install_deps
 
