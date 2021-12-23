2baf9f1335f654fdfb02292f0f68cf0c8d02eb71
Oleg Pudeyev <code@olegp.name>
2021-12-23 11:09:03 -0500

    instantiate DBRefs under jruby

--- lib/bson/hash.rb
+++ lib/bson/hash.rb
@@ -128,6 +128,16 @@ module BSON
           if actual_byte_size != expected_byte_size
             raise Error::BSONDecodeError, "Expected hash to take #{expected_byte_size} bytes but it took #{actual_byte_size} bytes"
           end
+
+          if hash['$ref'] && hash['$id']
+            # We're doing implicit decoding here. If the document is an invalid
+            # dbref, we should decode it as a BSON::Document.
+            begin
+              hash = DBRef.new(hash)
+            rescue ArgumentError
+            end
+          end
+
           hash
         end
       end
