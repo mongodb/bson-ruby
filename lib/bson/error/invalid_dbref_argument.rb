095776909c32bb0ffb1a324220db1f5e33668740
Neil Shweky <neilshweky@gmail.com>
2022-09-12 13:04:53 -0400

    fix api private error

--- lib/bson/error/invalid_dbref_argument.rb
+++ lib/bson/error/invalid_dbref_argument.rb
@@ -2,9 +2,8 @@
 
 module BSON
   class Error
+    
     # Raised when trying to create a BSON::DBRef from an object that is an invalid DBRef.
-    #
-    # @api private
     class InvalidDBRefArgument < Error
     end
   end
