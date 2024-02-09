88ac9e410936089a791d2b1aeae1dd344a883df5
Jamis Buck <jamisbuck@gmail.com>
2024-02-09 08:38:50 -0700

    version bump (#322)

--- release/jruby/install.sh
+++ release/jruby/install.sh
@@ -8,4 +8,4 @@ cd /rubies
 git clone https://github.com/rbenv/ruby-build.git
 PREFIX=/usr ./ruby-build/install.sh
 
-ruby-build -v jruby-9.2.11.1 /rubies/jruby
+ruby-build -v jruby-9.3.13.0 /rubies/jruby
