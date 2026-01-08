2e24b9841c30db589831d8978609dc464f872f89
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2026-01-08 10:38:38 +0100

    Use new toolchain

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -13,7 +13,15 @@ set -o errexit  # Exit the script with error if any of the commands fail
 
 set_env_vars
 
-set_env_ruby
+# Install rbenv and download the requested ruby version
+rm -rf ~/.rbenv
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+rm -rf ~/.rbenv/versions/
+curl --retry 3 -fL http://boxes.10gen.com/build/toolchain-drivers/mongo-ruby-toolchain/library/`host_distro`/$RVM_RUBY.tar.xz |tar -xC $HOME/.rbenv/ -Jf -
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init - bash)"
+export FULL_RUBY_VERSION=$(ls ~/.rbenv/versions | head -n1)
+rbenv global $FULL_RUBY_VERSION
 
 install_deps
 
